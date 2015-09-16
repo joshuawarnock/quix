1bb5ed8c108b51827aabd078b829c98a5dd13cc2
mugillicutty <joshua.warnock@gmail.com>
2015-09-16 14:07:03 -0700

    router path one

--- js/app.js
+++ js/app.js
@@ -1,42 +1,41 @@
 /**
  * Created by joshuawarnock on 9/16/15.
  */
-
 //This is the server
 var express = require('express');
 var app = express();
 var router = express.Router();
 var path = require('path');
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
+//var bodyParser = require('body-parser');
+//var jsonParser = bodyParser.json();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(function(req, res, next){
-  console.log(req.url);
-  next();
-});
+//// parse application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({ extended: false }));
+//
+//app.use(function(req, res, next){
+//  console.log(req.url);
+//  next();
+//});
 
 // INDEX PATH
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '../index.html'));
 });
-// APP PATH
-app.get('/app', function(req, res) {
-  res.sendFile(path.join(__dirname + '/app.js'));
-});
-// FORM PATH
-app.get('/form.js', function(req, res) {
-  res.sendFile(path.join(__dirname + '/form.js'));
-});
+//// APP PATH
+//app.get('/app', function(req, res) {
+//  res.sendFile(path.join(__dirname + '/app.js'));
+//});
+//// FORM PATH
+//app.get('/form.js', function(req, res) {
+//  res.sendFile(path.join(__dirname + '/form.js'));
+//});
 
-app.post('/form',jsonParser, function(req,res){
-  console.log(JSON.stringify(req.body));
-  res.send('Form has been submitted');
-});
+//app.post('/form',jsonParser, function(req,res){
+//  console.log(JSON.stringify(req.body));
+//  res.send('Form has been submitted');
+//});
 
 // start the server
-var server = app.listen(1338, function(){
+var server = app.listen(1400, function(){
   console.log('Server running at http://localhost:' + server.address().port);
 });
--- js/form.js
+++ js/form.js
@@ -2,5 +2,36 @@
  * Created by joshuawarnock on 9/16/15.
  */
 
-var emailInput = document.getElementById()
+var emailInput = document.getElementById('emailinput');
+var passwordInput = document.getElementById('passwordinput');
+
+function checkEmail(usernameInput){
+  var mssG = document.getElementById('empty-username-div');
+  if (usernameInput == 'bond'){
+    mssG.textContent = "Username Ok";
+  } else {
+    mssG.textContent = "Username Bad";
+  }
+}
+
+function checkPassword(passwordInput){
+  var mssG = document.getElementById('empty-password-div');
+  if (passwordInput == 'secret'){
+    mssG.textContent = "Password Ok.";
+  } else {
+    mssG.textContent = "Password Bad";
+  }
+}
+
+emailInput.addEventListener('blur', function(){
+  var userEmail = document.getElementById('emailinput');
+  checkEmail(userEmail.value);
+}, false);
+
+passwordInput.addEventListener('blur', function(){
+  var userPassword = document.getElementById('passwordinput');
+  checkPassword(userPassword.value);
+}, false);
+
+
 
