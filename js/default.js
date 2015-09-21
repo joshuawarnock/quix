baa7a69c03525a0650a6c2ea7473ae1051518f5c
mugillicutty <joshua.warnock@gmail.com>
2015-09-21 11:11:00 -0700

    search feature and tile hide-show code #1

--- js/default.js
+++ js/default.js
@@ -19,167 +19,6 @@ $( function() {
     $container.packery( 'bindDraggabillyEvents', draggie );
   });
 });
-// Prepend Tile Begin-------------------------------------------------------------|
-// Account Tile
-$( function() {
-  var $container = $('.packery').packery();
-  $('#account-button').on( 'click', function() {
-    // create new item elements
-    var elems = [];
-    for ( var i = 0; i < 1; i++ ) {
-      var elem = MakeAccountTile();
-      elems.push( elem );
-    }
-    // append elements to container
-    $container.prepend( elems );
-    // add and lay out newly appended elements
-    $container.packery( 'prepended', elems );
-  });
-});
-// Vehicle Tile
-$( function() {
-  var $container = $('.packery').packery();
-  $('#vehicle-button').on( 'click', function() {
-    // create new item elements
-    var elems = [];
-    for ( var i = 0; i < 1; i++ ) {
-      var elem = VehicleTileMaker();
-      elems.push( elem );
-    }
-    // append elements to container
-    $container.prepend( elems );
-    // add and lay out newly appended elements
-    $container.packery( 'prepended', elems );
-  });
-});
-// Manual Tile
-$( function() {
-  var $container = $('.packery').packery();
-  $('#manual-button').on( 'click', function() {
-    // create new item elements
-    var elems = [];
-    for ( var i = 0; i < 1; i++ ) {
-      var elem = ManualTileMaker();
-      elems.push( elem );
-    }
-    // append elements to container
-    $container.prepend( elems );
-    // add and lay out newly appended elements
-    $container.packery( 'prepended', elems );
-  });
-});
-// Shop Tile
-$( function() {
-  var $container = $('.packery').packery();
-  $('#shop-button').on( 'click', function() {
-    // create new item elements
-    var elems = [];
-    for ( var i = 0; i < 1; i++ ) {
-      var elem = ShopTileMaker();
-      elems.push( elem );
-    }
-    // append elements to container
-    $container.prepend( elems );
-    // add and lay out newly appended elements
-    $container.packery( 'prepended', elems );
-  });
-});
-// Video Tile
-$( function() {
-  var $container = $('.packery').packery();
-  $('#video-button').on( 'click', function() {
-    // create new item elements
-    var elems = [];
-    for ( var i = 0; i < 1; i++ ) {
-      var elem = VideoTileMaker();
-      elems.push( elem );
-    }
-    // append elements to container
-    $container.prepend( elems );
-    // add and lay out newly appended elements
-    $container.packery( 'prepended', elems );
-  });
-});
-// Search Tile
-$( function() {
-  var $container = $('.packery').packery();
-  $('#search-button').on( 'click', function() {
-    // create new item elements
-    var elems = [];
-    for ( var i = 0; i < 1; i++ ) {
-      var elem = SearchTileMaker();
-      elems.push( elem );
-    }
-    // append elements to container
-    $container.prepend( elems );
-    // add and lay out newly appended elements
-    $container.packery( 'prepended', elems );
-  });
-});
-// Account Tile Maker
-function MakeAccountTile() {
-  var elem = document.createElement('div');
-  elem.className = 'item w3 h3';
-  elem.id = 'account-tile';
-  //elem.background = "url('/images/my-account-icon.png') no-repeat center center";
-  elem.style.backgroundImage = "url('/images/my-account-icon.png')";
-  elem.style.backgroundRepeat = "no-repeat";
-  elem.style.verticalAlign = "middle";
-  return elem;
-}
-// Vehicle Tile Maker
-function VehicleTileMaker() {
-  var elem = document.createElement('div');
-  elem.className = 'item w3 h3';
-  elem.id = 'vehicles-tile';
-  elem.style.backgroundImage = "url('/images/my-vehicles-icon.png')";
-  elem.style.backgroundRepeat = "no-repeat";
-  return elem;
-}
-// Manual Tile Maker
-function ManualTileMaker() {
-  var elem = document.createElement('div');
-  elem.className = 'item w3 h3';
-  elem.id = 'manual-tile';
-  elem.style.backgroundImage = "url('/images/manual-icon-tran.png')";
-  elem.style.backgroundRepeat = "no-repeat";
-  return elem;
-}
-// Shop Tile Maker
-function ShopTileMaker() {
-  var elem = document.createElement('div');
-  elem.className = 'item w3 h3';
-  elem.id = 'shop-tile';
-  elem.style.backgroundImage = "url('/images/parts-shopping-icon.png')";
-  elem.style.backgroundRepeat = "no-repeat";
-  return elem;
-}
-// Video Tile Maker
-function VideoTileMaker() {
-  var elem = document.createElement('div');
-  elem.className = 'item w3 h3';
-  elem.id = 'video-tile';
-  elem.style.backgroundImage = "url('/images/play-icon.png')";
-  elem.style.backgroundRepeat = "no-repeat";
-  return elem;
-}
-// Search Tile Maker
-//function SearchTileMaker() {
-//  var elem = document.createElement('div');
-//  elem.className = 'item w3 h3';
-//  elem.id = 'search-tile';
-//  var elem2 = document.createElement('div');
-//  elem2.className = 'row center-block search-row';
-//  elem.appendChild('elem2');
-//  return elem;
-//}
-function SearchTileMaker() {
-  var elem = document.createElement('div');
-  elem.innerHTML = '<div class="item w3 h3"><div class="row center-block search-row"> <div class="col-md-12"> <div id="carousel-search-container"> <h3 class="text-center carousel-search-heading">Find your fix.</h3> <form> <div class="input-group stylish-input-group"> <input id="searchinput" type="text" class="form-control" placeholder="Search"> <span class="input-group-addon carousel-search-button"><button id="thesearchbutton" type="submit"><span class="glyphicon glyphicon-search"></span></button></span> </div> <div id="mtsearchdiv"></div> </form> </div> </div> </div> </div>';
-  return elem;
-}
-// Prepend Tile End---------------------------------------------|
-
 // Tile Expand Fit
 $( function() {
   var $container = $('.packery').packery();
@@ -197,7 +36,7 @@ $( function() {
 });
 var TheSearchButton = document.getElementById('thesearchbutton');
 function ResultResize(){
-  var ResultBox = document.getElementById('search-box');
+  var ResultBox = document.getElementById('search-tile');
   ResultBox.style.height = '1500px';
   ResultBox.style.width = '100%';
 }
@@ -206,6 +45,26 @@ TheSearchButton.addEventListener('click',function(e){
   var UserSearchClick = document.getElementById('return1title');
   ResultResize(TheSearchInput.value);
 },false);
+
+var TheAccountButton = document.getElementById('account-button');
+var TheAccountTile = document.getElementById('account-tile');
+
+var grabbedList = document.getElementById('nav-list');
+
+grabbedList.addEventListener('click', function(theEvent){
+  var content = theEvent.target.getAttribute('data-content');
+  checkTheclick(content);
+},false);
+
+function checkTheclick(contentId){
+  var section = document.getElementById(contentId);
+  if (section.style.display == 'block') {
+    section.style.display = 'none';
+  } else if (section.style.display == 'none' || section.style.display == '') {
+    section.style.display = 'block';
+  }
+}
+
 
 //$(document).ready(function() {
 //  $('#carousel-tile').carousel({
@@ -222,3 +81,5 @@ TheSearchButton.addEventListener('click',function(e){
 //    $(this).carousel('next');
 //  });
 //});
+
+
