5c59824a8ce2f7f9494e37e9994fd96a52c75903
mugillicutty <joshua.warnock@gmail.com>
2015-09-29 13:41:30 -0700

    refactored default js and removes some images not being used

--- js/app.js
+++ js/app.js
@@ -10,35 +10,22 @@ var api = require('./api.js');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
-app.use(express.static('../css'));
+
 app.use('/css',express.static('../css'));
-app.use(express.static('../fonts'));
 app.use('/fonts', express.static('../fonts'));
-app.use(express.static('../images'));
 app.use('/images',express.static('../images'));
-app.use(express.static('../js'));
 app.use('/js', express.static('../js'));
+
 app.use('/api',api);
 
 // Parse Application
 app.use(bodyParser.urlencoded({ extended: false }));
-//Print URL to console
-app.use(function(req, res, next){
-  console.log(req.url);
-  next();
-});
+
 // Index Path
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/../index.html'));
 });
-// Google API Path
-app.get('api', function(req, res) {
-  res.sendFile(path.join(__dirname + '/api.js'));
-});
-// Form Path
-app.get('/form.js', function(req, res) {
-  res.sendFile(path.join(__dirname + '/form.js'));
-});
+
 app.post('/form',jsonParser, function(req,res){
   console.log(JSON.stringify(req.body));
   res.send('Form has been submitted');
--- js/form.js
+++ js/form.js
@@ -4,36 +4,7 @@
 
 var TheSearchInput = document.getElementById('searchinput');
 var TheSearchButton = document.getElementById('thesearchbutton');
-var SearchReturn1Title = document.getElementById('return1title');
-var SearchReturn1Link = document.getElementById('return1link');
-var SearchReturn1Desc = document.getElementById('return1desc');
-var SearchReturn2Title = document.getElementById('return2title');
-var SearchReturn2Link = document.getElementById('return2link');
-var SearchReturn2Desc = document.getElementById('return2desc');
-var SearchReturn3Title = document.getElementById('return3title');
-var SearchReturn3Link = document.getElementById('return3link');
-var SearchReturn3Desc = document.getElementById('return3desc');
-var SearchReturn4Title = document.getElementById('return4title');
-var SearchReturn4Link = document.getElementById('return4link');
-var SearchReturn4Desc = document.getElementById('return4desc');
-var SearchReturn5Title = document.getElementById('return5title');
-var SearchReturn5Link = document.getElementById('return5link');
-var SearchReturn5Desc = document.getElementById('return5desc');
-var SearchReturn6Title = document.getElementById('return6title');
-var SearchReturn6Link = document.getElementById('return6link');
-var SearchReturn6Desc = document.getElementById('return6desc');
-var SearchReturn7Title = document.getElementById('return7title');
-var SearchReturn7Link = document.getElementById('return7link');
-var SearchReturn7Desc = document.getElementById('return7desc');
-var SearchReturn8Title = document.getElementById('return8title');
-var SearchReturn8Link = document.getElementById('return8link');
-var SearchReturn8Desc = document.getElementById('return8desc');
-var SearchReturn9Title = document.getElementById('return9title');
-var SearchReturn9Link = document.getElementById('return9link');
-var SearchReturn9Desc = document.getElementById('return9desc');
-var SearchReturn10Title = document.getElementById('return10title');
-var SearchReturn10Link = document.getElementById('return10link');
-var SearchReturn10Desc = document.getElementById('return10desc');
+
 function SubmitSearch() {
   console.log(SearchFormData());
   var SearchClicked = new XMLHttpRequest();
@@ -41,38 +12,27 @@ function SubmitSearch() {
   SearchClicked.setRequestHeader('Content-type','application/json');
   SearchClicked.send();
   SearchClicked.addEventListener('load',function(){
-    var Results = JSON.parse(SearchClicked.responseText);
-    for (var i = 0; i < Results.length; i++){
-      SearchReturn1Title.textContent = Results[0].title;
-      SearchReturn1Link.textContent = Results[0].link;
-      SearchReturn1Desc.textContent = Results[0].description;
-      SearchReturn2Title.textContent = Results[1].title;
-      SearchReturn2Link.textContent = Results[1].link;
-      SearchReturn2Desc.textContent = Results[1].description;
-      SearchReturn3Title.textContent = Results[2].title;
-      SearchReturn3Link.textContent = Results[2].link;
-      SearchReturn3Desc.textContent = Results[2].description;
-      SearchReturn4Title.textContent = Results[3].title;
-      SearchReturn4Link.textContent = Results[3].link;
-      SearchReturn4Desc.textContent = Results[3].description;
-      SearchReturn5Title.textContent = Results[4].title;
-      SearchReturn5Link.textContent = Results[4].link;
-      SearchReturn5Desc.textContent = Results[4].description;
-      SearchReturn6Title.textContent = Results[5].title;
-      SearchReturn6Link.textContent = Results[5].link;
-      SearchReturn6Desc.textContent = Results[5].description;
-      SearchReturn7Title.textContent = Results[6].title;
-      SearchReturn7Link.textContent = Results[6].link;
-      SearchReturn7Desc.textContent = Results[6].description;
-      SearchReturn8Title.textContent = Results[7].title;
-      SearchReturn8Link.textContent = Results[7].link;
-      SearchReturn8Desc.textContent = Results[7].description;
-      SearchReturn9Title.textContent = Results[8].title;
-      SearchReturn9Link.textContent = Results[8].link;
-      SearchReturn9Desc.textContent = Results[8].description;
-      SearchReturn10Title.textContent = Results[9].title;
-      SearchReturn10Link.textContent = Results[9].link;
-      SearchReturn10Desc.textContent = Results[9].description;
+    var results = JSON.parse(SearchClicked.responseText);
+    var resultList = document.getElementById('search-return-area');
+    for (var i = 0; i < results.length; i++) {
+      var result = document.createElement('div');
+      var title = document.createElement('div');
+      var link = document.createElement('div');
+      var description = document.createElement('div');
+
+      result.setAttribute('class', 'well');
+      title.textContent = results[i].title;
+      title.setAttribute('class', 'result-title');
+      link.textContent = results[i].link;
+      link.setAttribute('class', 'result-link');
+      link.setAttribute('href', results[i].link);
+      description.textContent = results[i].description;
+      description.setAttribute('class', 'result-description');
+
+      result.appendChild(title);
+      result.appendChild(link);
+      result.appendChild(description);
+      resultList.appendChild(result);
     }
   });
 }
