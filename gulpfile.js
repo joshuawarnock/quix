bec7cc9c7e5108273c20d40fc06681f670c01f6a
mugillicutty <joshua.warnock@gmail.com>
2015-10-01 15:27:33 -0700

    uglify, minify-html and minify-css complete in new public folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var nodemon = require('gulp-nodemon');
 var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+
 
 gulp.task('start', function() {
   nodemon({
@@ -15,18 +18,34 @@ gulp.task('start', function() {
 
 gulp.task('test', function() {
   return gulp.src('test.js', {read: false})
-      .pipe(mocha({reporter: 'landing'}));
+      .pipe(mocha());
 });
 
 gulp.task('watch', function() {
   gulp.watch('*.js', ['test']);
 });
 
-gulp.task('compress', function() {
-  return gulp.src('quix/*.js')
+gulp.task('uglify', function() {
+  return gulp.src('js/*.js')
       .pipe(uglify())
       .pipe(gulp.dest('public/dist/'));
 });
 
-gulp.task('default', ['test', 'watch', 'compress']);
+gulp.task('minify-html', function() {
+  var opts = {
+    conditionals: true,
+    spare: true,
+    comments: true
+  };
+  return gulp.src('*.html')
+      .pipe(minifyHTML(opts))
+      .pipe(gulp.dest('public/dist/'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+      .pipe(gulp.dest('public/dist/css'))
+});
+
+gulp.task('default', ['test', 'watch', 'uglify', 'minify-html', 'minify-css']);
 
--- test.js
+++ test.js
@@ -9,20 +9,9 @@ describe('Express Backend Test', function(){
 
   it('Page status OK.', function(done){
     request.get('/')
-    //agent
-        .get('/')
         .expect(200)
         .end(done)
   });
-  //it('Verify index.html header.', function(done){
-  //  agent
-  //      .get('/')
-  //      .set('Accept', 'text/html')
-  //      .expect('Content-Type', 'text/html; charset=UTF-8')
-  //      .end(function(err, res){
-  //        if (err) return done(err);
-  //        done();
-  //      })
-  //});
+
 });
 
